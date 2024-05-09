d01ef4d7eafba64b28f7cb9591f48361822347ab
whejy <john.wheldale@gmail.com>
2024-05-09 13:32:12 +0930

    13.22 Add route for editing 'read' status of reading list items

--- blogs/controllers/readingLists.js
+++ blogs/controllers/readingLists.js
@@ -1,10 +1,30 @@
 const router = require('express').Router();
 require('express-async-errors');
-const { ReadingList } = require('../models');
+const { ReadingList, User } = require('../models');
 
 router.post('/', async (req, res) => {
-    const list = await ReadingList.create({...req.body})
-    res.json(list)
+    const list = await ReadingList.create({ ...req.body });
+    res.json(list);
+});
+
+router.put('/:id', async (req, res) => {
+    const readingListBlog = await ReadingList.findByPk(req.params.id);
+
+    if (!readingListBlog)
+        return res.status(404).json({
+            error: `Reading list with id ${req.params.id} not found`,
+        });
+
+    if (readingListBlog.userId !== req.decodedToken.id)
+        return res.status(401).json({
+            error: 'User not authorized to edit this reading list',
+        });
+
+    if (typeof req.body.read === 'boolean') {
+        readingListBlog.read = req.body.read;
+        await readingListBlog.save();
+    }
+    res.json(readingListBlog);
 });
 
 module.exports = router;
