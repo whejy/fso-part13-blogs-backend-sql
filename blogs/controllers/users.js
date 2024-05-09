9d53ec517a2158e877fd4b023b51626dd3bd6b50
whejy <john.wheldale@gmail.com>
2024-05-09 12:28:21 +0930

    13.21 Add 'read' to SQL query for User reading list

--- blogs/controllers/users.js
+++ blogs/controllers/users.js
@@ -33,7 +33,7 @@ router.get('/:id', async (req, res) => {
                 as: 'readings',
                 attributes: { exclude: ['userId', 'createdAt', 'updatedAt'] },
                 through: {
-                    attributes: [],
+                    attributes: ['read', 'id'],
                 },
             },
         ],
